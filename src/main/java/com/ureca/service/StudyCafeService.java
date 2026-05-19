001cfd2636e133070e49f05305f9a150e0d6b795
jimizip <9koorry9@gmail.com>
2026-05-19 17:10:20 +0900

    modified interface

--- src/main/java/com/ureca/service/StudyCafeService.java
+++ src/main/java/com/ureca/service/StudyCafeService.java
@@ -1,10 +1,8 @@
 package com.ureca.service;
 
 import java.util.List;
-import com.ureca.dto.User;
-import com.ureca.dto.PaymentHistory;
-// import com.ureca.dto.RoomHistory;
-// import com.ureca.dto.Room;
+import java.time.LocalDateTime;
+import com.ureca.dto.*;
 
 public interface StudyCafeService {
 
@@ -13,13 +11,16 @@ public interface StudyCafeService {
     List<User> searchAllUsers();                // 유저 전체 조회
     User searchUser(int id);
 
-    // 나중에 추가 - 예약 예시
-    // void reserve(RoomHistory history);          // 예약 등록 
-    // void updateReservation(RoomHistory history);// 예약 수정
-    // void cancel(int historyId);                 // 예약 취소
-    // List<RoomHistory> searchAllHistory();       // 전체 예약 조회
-    // List<RoomHistory> searchHistoryByUser(int userId); // 유저별 예약 조회
-    // List<Room> searchAvailableRooms(java.time.LocalDateTime start, java.time.LocalDateTime end); // 예약 가능 룸 조회
+    // 예약 - 현빈
+    void reserve(Room_history history); // 예약 등록
+    List<Room> searchAvailableRooms(LocalDateTime start, LocalDateTime end); // 날짜 기준 예약 가능 룸 조회
+    List<Integer> getBookedHours(int roomId, LocalDateTime date); // 룸, 날짜 기준 예약 가능 시간 조회
+
+    // 예약 - 지민
+    void updateReservation(Room_history history); // 예약 수정
+    void cancel(int historyId); // 예약 취소
+    List<Room_history> searchHistoryByUser(int userId); // 유저별 예약 조회 
+    List<Room_history> searchAllHistory(); // 전체 예약 조회
 
     // 결제
     List<PaymentHistory> searchPaymentByUser(int userId); // 유저별 결제 내역 조회
--- src/main/java/com/ureca/util/StudyCafeFactory.java
+++ src/main/java/com/ureca/util/StudyCafeFactory.java
@@ -1,9 +1,6 @@
 package com.ureca.util;
 
-import com.ureca.dao.UserDao;
-import com.ureca.dao.UserDaoImp;
-import com.ureca.dao.PaymentHistoryDao;
-import com.ureca.dao.PaymentHistoryDaoImp;
+import com.ureca.dao.*;
 
 // DAO 객체 하나씩만 생성해서 재사용
 public class StudyCafeFactory {
@@ -13,12 +10,9 @@ public class StudyCafeFactory {
     public static UserDao getUserDao() { return userDao; }
     public static PaymentHistoryDao getPaymentHistoryDao() { return paymentHistoryDao; }
     
-    // 나중에 추가
-    /**
-    private static final RoomDao roomDao = new RoomDaoImp();
-    private static final RoomHistoryDao roomHistoryDao = new RoomHistoryDaoImp();
+    private static final RoomDao roomDao = new RoomDaolmp();
+    private static final RoomHistoryDao roomHistoryDao = new RoomHistoryDaolmp();
 
     public static RoomDao getRoomDao() { return roomDao; }
     public static RoomHistoryDao getRoomHistoryDao() { return roomHistoryDao; }
-    **/
 }
