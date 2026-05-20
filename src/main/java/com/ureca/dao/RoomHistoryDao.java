696a451dfab71767dfff76acbde83d52f0f35c8a
pc <pc@220.119.83.136>
2026-05-20 14:50:21 +0900

    주석 추가

--- src/main/java/com/ureca/dao/RoomHistoryDao.java
+++ src/main/java/com/ureca/dao/RoomHistoryDao.java
@@ -8,13 +8,14 @@ import java.sql.Connection;
 import com.ureca.dto.Room_history;
 public interface RoomHistoryDao {
 	
+	//해당 room의 예약 정보 조회
 	List<Room_history> getReservation(int room_id,LocalDateTime date) throws SQLException;
-	
+	//해당 유저의 예약 정보 조회
 	List<Room_history> getHistory(int user_id) throws SQLException;
-	
+	//예약 설정
     int setReserve(Room_history reserve, Connection con) throws SQLException;
-
+    //예약 수정
 	void updateReserve(int id, Room_history reserve, Connection con) throws SQLException;
-	
+	//예약 삭제
 	void removeReserve(int id) throws SQLException;
 }
