8e2ea8015956c2e6ca04d3ba676926c9f42faed5
jimizip <9koorry9@gmail.com>
2026-06-23 15:35:21 +0900

    refactor: 기존 Swing 데스크톱/순수 JDBC   코드 제거

--- src/main/java/com/ureca/dto/CanNotFindException.java
+++
@@ -1,9 +0,0 @@
-package com.ureca.dto;
-
-// 유저, 룸, 예약 등 데이터를 찾지 못했을 때 예외 처리
-public class CanNotFindException extends RuntimeException {
-	public CanNotFindException() {}
-    public CanNotFindException(String msg) {
-        super(msg);
-    }
-}
